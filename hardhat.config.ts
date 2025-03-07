87bed44b5edff8435cc49ebdcceb7fab12c1d5a5
0xRoy <1997roylee@gmail.com>
2025-03-08 02:09:31 +0800

    init

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import { vars } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+  networks: {
+    monadTestnet: {
+      url: vars.get("MONAD_RPC_URL"),
+      accounts: [vars.get("PRIVATE_KEY")],
+      chainId: Number(vars.get("MONAD_CHAIN_ID")),
+  },
+  },
+  sourcify: {
+    enabled: true,
+    apiUrl: "https://sourcify-api-monad.blockvision.org",
+    browserUrl: "https://testnet.monadexplorer.com/"
+},
+// To avoid errors from Etherscan
+etherscan: {
+    enabled: false,
+},
+};
+
+export default config;
